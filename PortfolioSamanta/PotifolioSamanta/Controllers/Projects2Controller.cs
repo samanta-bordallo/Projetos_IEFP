ddc8eb0d755b96db0211cf3f404684aa066d323d
samanta-bordallo <bordallo.samanta@gmail.com>
2023-10-26 20:45:47 +0100

    functional state with button

---
+++ PortfolioSamanta/PotifolioSamanta/Controllers/Projects2Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioSamanta.Controllers
+{
+    public class Projects2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PortfolioSamanta/PotifolioSamanta/Models/Projects2Model.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PotifolioSamanta.Models
+{
+    public class Projects2Model
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter the project name!")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Is the project finished?!")]
+        public string? Finished { get; set; }
+        [Required(ErrorMessage = "Fill in the link!")]
+        public string? Link { get; set; }
+        public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+    }
+}
